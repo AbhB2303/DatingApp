9db119b7438f000487da99e9c8031bdbfd2680cb
Abhinav Batra <abhinavbatra2303@gmail.com>
2022-05-12 11:09:01 -0400

    End of Section 6

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -13,7 +15,7 @@ export class NavComponent implements OnInit {
   model: any = {} //class property to store info for two way binding
   //two way binding: info can be exchanged from component to html and vice versa
   //currentUser$: Observable<User>;
-  constructor(public accountService: AccountService) {
+  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) {
 
   }
 
@@ -23,16 +25,17 @@ export class NavComponent implements OnInit {
 
   login() {
     this.accountService.login(this.model).subscribe(Response => {
-      console.log(Response);
+      this.router.navigateByUrl("/members");
   
     }, error => {
       console.log(error);
+      this.toastr.error(error.error);
     })
   }
 
   logout() {
     this.accountService.logout();
-
+    this.router.navigateByUrl("/");
   }
 
  
