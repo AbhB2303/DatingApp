26d9775fec6e9b71bac18b4b4da5be20f1a8f649
Abhinav Batra <abhinavbatra2303@gmail.com>
2022-05-18 08:02:27 -0400

    learning typescript

--- client/src/app/_models/user.ts
+++ client/src/app/_models/user.ts
@@ -5,3 +5,37 @@ export interface User {
     token: string;
 
 }
+
+// typescript basics:
+
+// define variable: let x = 42
+// automatically determines type
+// can assign two types:
+// let data: number | string = 42;
+
+// could create an interface for an object
+// helps to define types for each property
+// interface car {
+//  color: string
+//  mode: string
+//  topSpeed?: number
+// }
+
+// const multiply = (x, y) => {
+//     color: 'red'
+//     model: 'Mercedes'
+//     topSpeed: 100
+// }
+
+// can also define an object directly with properties
+// no safety on type of properties
+// const car1 = {
+//    color: 'blue',
+//    model: 'BMW'
+// }
+//
+// an object of type car
+// const car2 : car = {
+//    color: 'blue',
+//    model: 'BMW'
+// }
